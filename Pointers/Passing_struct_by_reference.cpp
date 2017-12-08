6c96e9ffa60672f873fdd0da670b0de095ee2537
BYOA <34205945+BYOA@users.noreply.github.com>
2017-12-08 16:25:04 +1100

    Update Passing_struct_by_reference.cpp

--- Pointers/Passing_struct_by_reference.cpp
+++ Pointers/Passing_struct_by_reference.cpp
@@ -6,26 +6,29 @@ struct change {
   int number;
  };
 
- typedef change NumberChange;
+ typedef change NumberChange; //type def alias.
 
- void PassByReference (NumberChange *n);
+/*object.member
+pointer->member*/
+
+void PassByReference (NumberChange *n);
 
 int main ()
 {
-  NumberChange number1;
-  number1.number = 7;
+  NumberChange value;
+  value.number = 7;
 
-  NumberChange *ptr1 = &number1;
+  NumberChange *ptr = &value;
 
-  printf("The value of number1 before function passing = %d\n", number1.number);
-  printf("The value of ptr1 before function = %d\n",ptr1->number);
+  printf("The value of number1 before function passing = %d\n", value.number);
+  printf("The value of ptr1 before function = %d\n",ptr->number);
 
-    PassByReference(ptr1);
+  PassByReference(ptr);
 
-    printf("The value of number1 after function passing = %d\n", number1.number);
-    printf("The value of ptr1 after function passing = %d\n",ptr1->number);
+  printf("The value of number1 after function passing = %d\n", value.number);
+  printf("The value of ptr1 after function passing = %d\n",ptr->number);
 
-    return 0 ; // successfully ran program
+  return 0 ; // successfully ran program
 }
 
 
