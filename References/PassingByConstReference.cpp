776c2e10a7d4779dee5efb8243365dfca93cc793
BYOA <34205945+BYOA@users.noreply.github.com>
2017-12-12 22:30:45 +1100

    Update PassingByConstReference.cpp

--- References/PassingByConstReference.cpp
+++ References/PassingByConstReference.cpp
@@ -12,15 +12,17 @@ int main ()
 {
     int value = 8;
     int value2 = 9;
+    const int value3 = 0;
     int &ref2 = value2;
     const int &constref = 8 + 3;
 
-    PassingByConstReference (value);
-    PassingByConstReference (value2 +value);
-    PassingByConstReference (ref2);
-    PassingByConstReference (constref);
-    PassingByConstReference (7);
-    PassingByConstReference (1+1);
+    PassingByConstReference (value); //non constant value
+    PassingByConstReference (value2 +value); //2 non constant values
+    PassingByConstReference (value3); //constant value
+    PassingByConstReference (ref2); //constant reference
+    PassingByConstReference (constref); //constant reference
+    PassingByConstReference (7); //integer literal
+    PassingByConstReference (1+1); // 2 integers literals
 
 
     return 0;
