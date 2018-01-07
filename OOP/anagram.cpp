d693eefdbdc25869b60a201c72cdda1c798654d7
BYOA <34205945+BYOA@users.noreply.github.com>
2018-01-07 13:56:48 +1100

    Create anagram.cpp

---
+++ OOP/anagram.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+
+
+class Solution {
+private: //states
+    std::string s1;
+    std::string s2;
+    std::string characterSort(std::string s)
+    {
+        for (int i = 0; i < s.size();i++)
+        {
+            int smallest = i;
+            for (int current = i + 1; current<s.size();current++)
+            {
+                if (s[smallest] > s[current])
+                {
+                    smallest = current;
+                }
+            }
+            std::swap(s[i], s[smallest]);
+        }
+        return s;
+    }
+    
+    bool isAnagram() 
+    {
+        if (characterSort(s1) == characterSort(s2))
+            return true;
+        else
+            return false;
+    }
+public: //behaviours
+    Solution()
+    {
+        std::cout << "Enter string 1: \n";
+        std::cin >> s1;
+        std::cout << "Enter string 2: \n";
+        std::cin >> s2;
+    }
+    Solution(std::string s): s1{s} //member intializer list
+    {
+        std::cout << "String 1: " << this->s1 << " is already entered.\nEnter string 2: \n";
+        std::cin >> (*this).s2;
+    }
+    void display()
+    {
+
+        if (isAnagram())
+        {
+            std::cout << s1 << " and " << s2 << " is an anagram. ";
+        }
+        else
+        {
+            std::cout << s1 << " and " << s2 << " is NOT an anagram. ";
+        }
+    }
+    ~Solution()//destructor
+    {
+        std::cout << "\n\nobject is now being destroyed after this sentenance.\n";
+    }
+};
+
+
+
+int main()
+{
+
+    Solution s;
+    s.display();
+
+    return 0;
+}
